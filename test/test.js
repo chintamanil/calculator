0b814931f99baaee494f759329eff1fed4347d0c
chintamanil <chintamani.lonkar@gmail.com>
2016-10-10 12:06:40 -0700

    added test for evaluate method

---
+++ test/test.js
@@ -0,0 +1,22 @@
+(function() {
+
+    var chai = require('chai');
+    var expect = chai.expect;
+    var console = require('./../src/console.js');
+    var evaluate = require('./../src/module/evaluate.js');
+
+    describe('Calcuator Testing', function() {
+        describe('c', function() {
+
+            before(function() {
+            });
+
+            it('Check if add operation is working ', function() {
+            });
+
+
+        });
+
+    });
+
+})();
